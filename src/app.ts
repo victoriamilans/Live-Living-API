30cdf8c6a69c3e9e948b4e5efa1d0cc6ac283492
Victoria <victoria-milan@outlook.com>
2023-02-23 13:18:02 -0300

    feat: add AppError class

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,22 @@
-import express, { Application } from "express";
+import "reflect-metadata";
+import express from "express";
+import "express-async-errors";
+// import userRoutes from "./routes/users.routes";
+// import sessionRoutes from "./routes/session.routes";
+import { handleError } from "./errors";
+// import categoriesRoutes from "./routes/categories.routes";
+// import propertiesRoutes from "./routes/properties.routes";
+// import schedulesRoutes from "./routes/schedules.routes";
 
-const app: Application = express();
+const app = express();
+
 app.use(express.json());
+
+// app.use("/users", userRoutes);
+// app.use("/login", sessionRoutes);
+// app.use("/categories", categoriesRoutes);
+// app.use("/properties", propertiesRoutes);
+// app.use("/schedules", schedulesRoutes);
+app.use(handleError);
 
 export default app;
